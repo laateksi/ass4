501c0ef9a7143fd6496d3281f935b005cffe5c6c
laateksi <ltyysk@gmail.com>
2016-04-15 10:09:27 +0300

    Added id checking to delete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var express = require('express'),
     app = express();
 var bodyParser = require("body-parser");
-var uuid = require('node-uuid');    
+var uuid = require('node-uuid');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -29,26 +29,26 @@ connection.connect(function (err) {
 
 
 //GET---------------------------------------------------------------------------
-app.get('/warehouse', function(req, res){
+app.get('/warehouse', function (req, res) {
     var warehouseView = {
-        materials : "/warehouse/materials",
-        products : "/warehouse/products"
+        materials: "/warehouse/materials",
+        products: "/warehouse/products"
     };
     res.send(warehouseView);
 });
 
 app.get('/warehouse/materials', function (req, res) {
     var data = {
-        "Back to Warehouse" : "/warehouse",
+        "Back to Warehouse": "/warehouse",
         "error": 1,
         "Materials": ""
     };
 
     var materialsView = {
-        back_to_warehouse : "/warehouse",
-        products : "/warehouse/products"
+        back_to_warehouse: "/warehouse",
+        products: "/warehouse/products"
     };
-    
+
     connection.query("SELECT * from materials", function (err, rows, fields) {
         if (rows.length != 0) {
             data["error"] = 0;
@@ -63,7 +63,7 @@ app.get('/warehouse/materials', function (req, res) {
 
 app.get('/warehouse/products', function (req, res) {
     var data = {
-        "Back to Warehouse" : "/warehouse",
+        "Back to Warehouse": "/warehouse",
         "error": 1,
         "Products": ""
     };
@@ -93,7 +93,7 @@ app.post('/warehouse/materials', function (req, res) {
         // Test that attributes are not empty
         if (query.category.length != 0 && query.type.length != 0
             && query.quantity.length != 0 && query.supplier.length != 0 && query.arrival_date.length != 0) {
-          
+
             // Create an id and GET the attributes and store them into variables
             var id = uuid.v4() // id generated
             var category = query.category;
@@ -103,21 +103,21 @@ app.post('/warehouse/materials', function (req, res) {
             var date = query.arrival_date;
 
             // inserting data to mySQL
-            connection.query("INSERT INTO materials (material_id,category,type,quantity,supplier,arrival_date) VALUES ('"+id+"','"+category+"','"+type+"','"+quantity+"','"+supplier+"','"+date+"')", function (err, rows, fields) {
+            connection.query("INSERT INTO materials (material_id,category,type,quantity,supplier,arrival_date) VALUES ('" + id + "','" + category + "','" + type + "','" + quantity + "','" + supplier + "','" + date + "')", function (err, rows, fields) {
                 if (err) {
                     console.log("Error Adding data: " + err);
                     data = "Error Adding data";
                     //respond to postman
                     res.status(400).json(data); //400 Query not complete
                 } else {
-                    
+
                     console.log("Material Created Successfully");
                     data = "Material Created Successfully";
                     //respond to postman
                     res.status(201).json(data); //201: Created
                 }
             });
-            
+
         } else {
             console.log("Empty attributes!")
             data = "Bad request: Empty attributes!";
@@ -148,7 +148,7 @@ app.post('/warehouse/products', function (req, res) {
             var customer = query.customer;
             var date = query.arrival_date;
 
-            connection.query("INSERT INTO products (product_id,category,type,quantity,customer,arrival_date) VALUES ('"+id+"','"+category+"','"+type+"','"+quantity+"','"+customer+"','"+date+"')", function (err, rows, fields) {
+            connection.query("INSERT INTO products (product_id,category,type,quantity,customer,arrival_date) VALUES ('" + id + "','" + category + "','" + type + "','" + quantity + "','" + customer + "','" + date + "')", function (err, rows, fields) {
                 if (err) {
                     console.log("Error Adding data");
                     data = "Error Adding data";
@@ -183,19 +183,31 @@ app.delete('/warehouse/products', function (req, res) {
     if (query.hasOwnProperty('id')) {
         // Test id value is not empty
         if (query.id.length != 0) {
-
+            // Get the id 
             var id = query.id;
+            //Check the if the id is in the database 
+            var check_id = "SELECT product_id FROM products WHERE product_id='"+id+"'";
+            connection.query(check_id, function (err, rows, fields) {
+                //if id is found, the length of the rows is not 0 
+                if (rows.length != 0) {
+                    //id can be deleted
+                    connection.query("DELETE FROM products WHERE product_id='" + id + "'", function (err, rows, fields) {
+                        if (err) {
+                            console.log("Error Deleting data");
+                            data = "Error Deleting data";
+                            res.status(404).json(data); //Not found
+                        } else {
 
-            connection.query("DELETE FROM products WHERE product_id='" + id+"'", function (err, rows, fields) {
-                if (err) {
-                    console.log("Error Deleting data");
-                    data = "Error Deleting data";
-                    res.status(404).json(data); //Not found
+                            console.log("Product Deleted Successfully");
+                            data = "OK. Product Deleted Successfully.";
+                            res.status(200).json(data); //OK
+                        }
+                    });
+                    //id is not ofund in the database
                 } else {
-
-                    console.log("Product Deleted Successfully");
-                    data = "OK. Product Deleted Successfully.";
-                    res.status(200).json(data); //OK
+                    console.log("There is no product with id: " + id);
+                    data = "Bad request: there is no product with id: " + id;
+                    res.status(404).json(data); //Not found
                 }
             });
 
@@ -223,16 +235,26 @@ app.delete('/warehouse/materials', function (req, res) {
 
             var id = query.id;
 
-            connection.query("DELETE FROM materials WHERE material_id='" + id + "'", function (err, rows, fields) {
-                if (err) {
-                    console.log("Error Deleting data");
-                    data = "Error Deleting data";
-                    res.status(404).json(data); //Not found
-                } else {
+            var check_id = "SELECT material_id FROM materials WHERE material_id='" + id + "'";
+            connection.query(check_id, function (err, rows, fields) {
+                if (rows.length != 0) {
 
-                    console.log("Material Deleted Successfully");
-                    data = "Material Deleted Successfully";
-                    res.status(200).json(data); //OK
+                    connection.query("DELETE FROM materials WHERE material_id='" + id + "'", function (err, rows, fields) {
+                        if (err) {
+                            console.log("Error Deleting data");
+                            data = "Error Deleting data";
+                            res.status(404).json(data); //Not found
+                        } else {
+
+                            console.log("Material Deleted Successfully");
+                            data = "OK. Material Deleted Successfully";
+                            res.status(200).json(data); //OK
+                        }
+                    });
+                } else {
+                    console.log("There is no material with id: " + id);
+                    data = "Bad request: there is no material with id: " + id;
+                    res.status(404).json(data); //Not found
                 }
             });
 
@@ -250,6 +272,7 @@ app.delete('/warehouse/materials', function (req, res) {
 
 //----Delete------------------------------------------------------------------------------
 
-    app.listen(80, function() {
-        console.log('Server started.');}
-    );
+app.listen(80, function () {
+    console.log('Server started.');
+}
+);
